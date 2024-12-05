7245a32f9d6efafd050a5ae00fc25e7dfa5305fe
Akash Shaw <136084773+akash-shaw@users.noreply.github.com>
2024-12-05 23:55:47 +0530

    SOLVE: 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if(!Number.isInteger(year)) return "ERROR";
+    if(year<0)  return "ERROR";
 
+    let isLeapyear=false;
+
+    if(year%100==0){
+        if(year%400==0) isLeapyear=true;
+    }
+    else{
+        if(year%4==0)   isLeapyear = true;
+    }
+    return isLeapyear;
 };
 
 // Do not edit below this line
