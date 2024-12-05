74189f59029cded2d347634f59e2ab6562d7e236
Akash Shaw <136084773+akash-shaw@users.noreply.github.com>
2024-12-05 23:50:43 +0530

    SOLVE: 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(lowerLimit, upperLimit) {
+    if(lowerLimit<0||upperLimit<0)  return "ERROR";
+    // if(Math.floor(lowerLimit)!=lowerLimit||Math.floor(upperLimit)!=upperLimit)  return "ERROR";
+    // if(typeof(lowerLimit)!=typeof(5)||typeof(upperLimit)!=typeof(5)) return "ERROR";
+    if(!Number.isInteger(upperLimit)||!Number.isInteger(lowerLimit)) return "ERROR";
+    
+    let sum = 0;
+    for(let i=Math.min(upperLimit,lowerLimit);i<=Math.max(upperLimit,lowerLimit);i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
