9caec4eadc791e85406f5c93b90231faa427765a
Akash Shaw <136084773+akash-shaw@users.noreply.github.com>
2024-12-05 23:40:50 +0530

    SOLVE: 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let newArr = [];
+    for(item of arr){
+        if(args.includes(item)){
+            continue;
+        }
+        else{
+            newArr.push(item);
+        }
+    }
+    return newArr;
 };
 
 // Do not edit below this line
