07d994c624c5e0b9b56e42fa570c48dc8489ea9c
Akash Shaw <136084773+akash-shaw@users.noreply.github.com>
2024-12-18 16:41:34 +0530

    SOLVE: 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    return cleanStr(str).toLowerCase() == reverseStr(cleanStr(str)).toLowerCase();
 };
+
+function cleanStr(str){
+    return str
+            .split("")
+            .filter( c=>{return (c>="A" && c<="Z") || (c>="a" && c<="z") || (c>="0" && c<="9");})
+            .join("");
+}
+function reverseStr (str){
+    return str.split("")
+            .reverse()
+            .join("");
+}
+
+console.log(reverseStr("Racecar!").toLowerCase());
+console.log("Racecar!".toLowerCase());
+console.log(palindromes("Racecar!"))
 
 // Do not edit below this line
 module.exports = palindromes;
