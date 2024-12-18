5d5c4ba430002a796423a14c860e2be1b91765e5
Akash Shaw <136084773+akash-shaw@users.noreply.github.com>
2024-12-18 16:54:50 +0530

    SOLVE: 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if(typeof n != 'number')n=parseInt(n);
+    if(n<0) return "OOPS";
+    if(n==0) return 0;
+    if(n==1 || n==2) return 1;
+    let former=1;
+    let later=1;
+    let count = 2;
+    while(count<n){
+        let sum = former + later;
+        former = later;
+        later = sum;
+        count++;
+    }
+    return later;
 };
 
 // Do not edit below this line
